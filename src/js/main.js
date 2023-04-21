0aeb194c55326028d8e6b98cca0052354b96ee71
Khausta <anna.a.khaustova@gmail.com>
2023-04-21 18:53:10 +0300

    site was published on link-host (http://massage.khausta.ru/)

--- src/js/main.js
+++ src/js/main.js
@@ -39,4 +39,93 @@ $(document).ready(function(){
         
         }
     });
+
+
+    //call
+  $('.overlay .modal__close').on('click', function() {
+    $('.overlay, #thanks').fadeOut();
   });
+
+  //validation
+  function valideForms(form) {
+    $(form).validate({
+      rules: {
+        name: {
+          required: true,
+          minlength: 2
+        },
+        phone: "required",
+        text: {
+          required: false,
+          maxlength: 200
+        },
+        checked: {
+          required: true
+        }
+      },
+      messages: {
+        name: {
+          required: "Пожалуйста, введите Ваше имя",
+          minlength: jQuery.validator.format("Введите {0} символа и более")
+        },
+        phone: "Пожалуйста, введите Ваш номер телефона",
+        checked:  {
+          required: "Пожалуйста, ознакомьтесь с Политикой конфиденциальности"
+        },
+        errorPlacement: function (error, element) {
+          if (element.attr("type") == "checkbox") {
+              return element.next('label').append(error);
+          }
+      
+           error.insertAfter($(element));
+        }
+
+      }
+    });
+  }
+  valideForms('form');
+
+  $('input[name=phone]').mask("+7 (999) 999-99-99");
+    
+  $('form').submit(function(e) {
+    e.preventDefault();
+
+    if (!$(this).valid()) {
+      return;  
+    }
+
+      $.ajax({
+        type: "POST",
+        url: "../mailer/smart.php",
+        data: $(this).serialize()
+      }).done(function() {
+        $(this).find("input").val("");
+        $('.overlay, #thanks').fadeIn('slow');
+        $('form').trigger('reset');
+      });
+      return false;
+    });
+  });
+
+  window.addEventListener('DOMContentLoaded', () => {
+    const hamburger = document.querySelector('.header__menu_hamburger'),
+          menu = document.querySelector('.menu_list-top'),
+          menuItem = document.querySelectorAll('.menu_list-top ul li.menuItem');
+         
+
+    hamburger.addEventListener('click', function() {
+        hamburger.classList.toggle('header__menu_hamburger-active');
+        menu.classList.toggle('menu_list-top-active');
+      });    
+        menuItem.forEach( item => { 
+            item.addEventListener('click', () => {
+            hamburger.classList.toggle('header__menu_hamburger-active');
+            menu.classList.toggle('menu_list-top-active');
+          });
+      });   
+  });
+
+  
+
+
+
